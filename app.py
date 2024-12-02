8b412dc026e64ad0317f2cf5704651ebea30ddd2
djennad <sadakdjennad@gmail.com>
2024-12-02 03:43:09 +0100

    Switch to direct Python execution for deployment

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from flask_socketio import SocketIO, emit, join_room, leave_room
 import secrets
 import os
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = secrets.token_hex(16)
-socketio = SocketIO(app, cors_allowed_origins="*")
+socketio = SocketIO(app, cors_allowed_origins="*", logger=True, engineio_logger=True)
 
 # Store waiting users
 waiting_users = []
@@ -48,5 +48,6 @@ def on_disconnect():
     waiting_users = [user for user in waiting_users if user != request.sid]
 
 if __name__ == '__main__':
-    port = int(os.environ.get("PORT", 5000))
-    socketio.run(app, host='0.0.0.0', port=port)
+    port = int(os.environ.get("PORT", 10000))
+    print(f"Starting server on port {port}")
+    socketio.run(app, host='0.0.0.0', port=port, debug=True, allow_unsafe_werkzeug=True)
